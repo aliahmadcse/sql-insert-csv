8eab023c0818dc270660bf5f3bfd463c34634503
aliahmadcse <aliahmadcse@gmail.com>
2020-04-05 18:34:13 +0500

    Adds script for writing into sql

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import pyodbc
+import csv
+
 
 conn = pyodbc.connect(
     "Driver={ODBC Driver 13 for SQL Server};"
@@ -7,14 +9,33 @@ conn = pyodbc.connect(
     "Trusted_Connection=yes;"
 )
 
+
 def read(conn):
     print("read")
-    cursor=conn.cursor()
-    cursor.execute("SELECT * FROM friends")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM movies")
     for row in cursor:
         print(row)
 
 
+def write(conn):
+    cursor = conn.cursor()
+    with open("movies.csv", "r") as file:
+        reader = csv.reader(file)
+        firstLine = True
+        for row in reader:
+            if firstLine:
+                firstLine = False
+                continue
+            sql = f"""INSERT INTO movies (id, name, genre, year, imdb_rating) VALUES (
+                {int(row[0])}, '{row[1]}', '{row[2]}', {0 if row[3]=='' else int(row[3])},{0 if row[4]=='' else float(row[4])}
+                );"""
+            print(sql)
+            cursor.execute(sql)
+        conn.commit()
+
+
+write(conn)
 read(conn)
 
 conn.close()
